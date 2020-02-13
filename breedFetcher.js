63ebd8f89028f69e5cab6765599f1dc04fd7bce8
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-13 17:26:09 +0000

    basic setup with Cat API

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,13 @@
 const request = require('request');
+
+const BASE_URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
+const breed = process.argv[2];
+
+request(BASE_URL + breed, (error, response, body) => {
+  if (error) {
+    console.log('Something went wrong!', error);
+    return;
+  }
+
+  console.log(JSON.parse(body)[0]);
+})
